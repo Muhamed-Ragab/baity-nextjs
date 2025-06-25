d825467074ff0e80d5f17a4f42a35b97bea217a7
Muhamed-Ragab <mohamedragab1649@gmail.com>
2025-06-25 23:58:34 +0300

    fix: Correct translation keys and update Cypress baseUrl
    This commit addresses the following:
    
    - **Corrected translation keys in `messages/en.json`:** Removed unnecessary suffixes ("- khara") from several translation keys to ensure accurate and consistent display of messages throughout the application.
    - **Updated Cypress baseUrl in `cypress.config.ts`:** Set the `baseUrl` to `http://localhost:3000` to avoid repetition in test files and improve maintainability.
    - **Removed redundant BASE_URL variable in `cypress/e2e/auth.cy.ts`:** Removed the unnecessary BASE_URL variable and used relative paths, as the base URL is now configured in `cypress.config.ts`.
    - **Deleted screenshot:** Removed an unnecessary screenshot file.

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -1,8 +1,6 @@
-const BASE_URL = 'http://localhost:3000';
-
 describe('auth', () => {
   it('register', () => {
-    cy.visit(`${BASE_URL}/auth/register`);
+    cy.visit('/auth/register');
 
     cy.get('input[name="email"]').type('test@example.com');
     cy.get('input[name="password"]').type('123456aA');
@@ -12,23 +10,23 @@ describe('auth', () => {
     cy.intercept('POST', '**/auth/register').as('register');
     cy.get('form').submit().wait('@register');
 
-    cy.get('div > p').then(($p) => {
+    cy.get('div > p').then($p => {
       if (!$p.text().match(/User already exists/i)) {
-        cy.url().should('eq', `${BASE_URL}/`);
+        cy.url().should('eq', '/');
         return;
       }
 
-      cy.visit(`${BASE_URL}/auth/login`);
+      cy.visit('/auth/login');
       cy.get('input[name="email"]').type('test@example.com');
       cy.get('input[name="password"]').type('123456aA');
 
       cy.intercept('POST', '**/auth/login').as('login');
       cy.get('form').submit().wait('@login');
-      cy.url().should('eq', `${BASE_URL}/`);
+      cy.url().should('eq', '/');
     });
   });
 
-  Cypress.on('uncaught:exception', (err) => {
+  Cypress.on('uncaught:exception', err => {
     if (err.message.includes('NEXT_REDIRECT')) {
       return false;
     }
